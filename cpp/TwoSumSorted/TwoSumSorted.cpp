872cc8105ce7d7766ac2ac12bc0177d5ee889324
Louis <louis@pop-os.localdomain>
2020-03-06 23:44:25 -0500

    Revision on logic, binary search seems faulty

--- cpp/TwoSumSorted/TwoSumSorted.cpp
+++ cpp/TwoSumSorted/TwoSumSorted.cpp
@@ -4,96 +4,64 @@
 using std::vector;
 using std::cout;
 
+
 int binarySearchClosest(vector<int>& numbers, int target){
     int L = 0;
     int R = numbers.size()-1;
-    int M = floor((L+R)/2);
-    int foundIndex;        
- 
-    for(int i=0;i<numbers.size();i++){
-        if(numbers[L] >= numbers[R]){//if L becomes R then take closest value
-            foundIndex = L;
+    int M; 
+    
+    while(L > R){//if L becomes R then take closest value
+        M = floor((L+R)/2);
+        if(target>numbers[M]){
+            L = M+1;
         }
-        else{
-            M = floor((L+R)/2);
-            if(target>numbers[M]){
-                L = M+1;
-            }
-            else if(target<numbers[M]){
-                R = M-1;
-            }
-            else {
-                cout<<"Found target!\n";
-                return M;
-            }
+        else if(target<numbers[M]){
+            R = M-1;
         }
+        else {
+            cout<<"Found target!\n";
+            return M;
+        }
+        
     }
-
-    return foundIndex;
+    cout<<"Failed binary search\n";
+    return -1;
 
 }
 
 
 vector<int> TwoSum(vector<int>& numbers, int target){
-    int firstIndex;
-    int secondIndex = binarySearchClosest(numbers, target);
-    vector<int> bigHit;
     
-  
+    vector<int> bigHit;
+    int subTarget, temp;
+    
+    for(int i=0;i<numbers.size();i++){
+        temp = target-numbers[i];
+        subTarget = binarySearchClosest(numbers, temp);
+        if(target - numbers[i] == numbers[subTarget]){
+            if(i>subTarget){
+                bigHit.push_back(subTarget);
+                bigHit.push_back(i);
 
-    if(numbers[secondIndex] == target){//if found target
-        
-        
-        if(numbers[secondIndex]<=0){
-            for(int i=0;i<numbers.size();i++){
-                if(numbers[i]==0){
-                    firstIndex = i;
-                    bigHit.push_back(firstIndex+1);
-                    break;
-
-                }
             }
+            else{
+                bigHit.push_back(i);
+                bigHit.push_back(subTarget);
+            }
+            break;
         }
-        bigHit.push_back(secondIndex+1);
-        
 
-        // return bigHit;
     }
+
+    cout<<"index 1: "<<bigHit[0]<<"\n";
+    cout<<"index 2: "<<bigHit[1]<<"\n";
 
    
 
-    else if(secondIndex==numbers.size()-1){
-        for(int i=0;i<secondIndex;i++){
-            if(numbers[i] == target-numbers[secondIndex] && i!=secondIndex){
-                firstIndex = i;
-            cout<<"firstIndex: "<<firstIndex<<"\n";
-            bigHit.push_back(firstIndex+1);
-            bigHit.push_back(secondIndex+1);
-            }
-        }
-    }
-
- 
-
-    else{
-        secondIndex--;
-        for(int i=0;i<=secondIndex;i++){
-        cout<<"\nblap "<<target-numbers[secondIndex];
-        if(numbers[i] == target-numbers[secondIndex] && i!=secondIndex){
-            firstIndex = i;
-            cout<<"firstIndex: "<<firstIndex<<"\n";
-            bigHit.push_back(firstIndex+1);
-            bigHit.push_back(secondIndex+1);
-        }
-
-        
-    }
-
-    }
     
-    cout<<"\n\nfirstIndex: "<<firstIndex<<" secondIndex: "<<secondIndex<<"\n";
     return bigHit;
 }
+
 
 
 int main(){
