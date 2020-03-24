c3ad3b42dd3a814f3fe201089c59e6dd146fa973
Louis <louisglhuynh@gmail.com>
2020-03-23 20:36:52 -0400

    missing tests ending in 11

--- cpp/67 - Add Binary/add_binary.cpp
+++ cpp/67 - Add Binary/add_binary.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-using std::string;
+#include <math.h>
+using std::string, std::to_string, std::cout;
 
 /*
 checklist:
@@ -10,50 +11,94 @@ checklist:
 -if carryOne && !addOne -> add 1 -> both false
 -if !carryOne && !addOne -> add 0 -> both false 
 */
-
 string addBinary(string a, string b) {
     int longer, shorter;
-    int bigBin, smallBin;
+    string* bigBin;
+    string* smallBin;
 
     if(a.length()>b.length()){
         longer = a.length();
         shorter = b.length();
+        bigBin = &a;
+        smallBin = &b;
+        
     }
     else{
         longer = b.length();
         shorter = a.length();
+        bigBin = &b;
+        smallBin = &a;
     }
+    
 
-    bool carryOn = false;
-    bool addOne = false;
-    int counter = 0;
+    int carryOne = 0;
     int buildBin = 0;
     for(int i=0;i<longer;i++){
-        if(i>shorter){
+        if(i>shorter-1){
             //only print long
-        }
-        else{
-            if(a[i] == '1' && b[i] == '1'){
-                //check if carryOn already true
-                carryOn = true;
-                
-            }
-            else if(a[i] == '1' && b[i] == '0'){
-                //just add one to new string
-                addOne = true;
-            }
-            else if(a[i] == '0' && b[i] == '1'){
-                //add one too
-                addOne = true;
+            if(i==longer-1){
+                if((*bigBin)[longer-1-i] == '1' && carryOne == 1){
+                    buildBin += pow(10, i+1);
+                }
             }
             else{
-                //add zero
-                addOne = false;
+                if((*bigBin)[longer-1-i] == '1'){
+                    if(carryOne == '0')
+                        buildBin += pow(10, i);
+                }
+            else if((*bigBin)[i] == '0'){
+                //do nothing
             }
+            else if((*bigBin)[i] == '1'){
+                 buildBin += pow(10, i);
+            }
+            else{
+                if(carryOne == '1')
+                    buildBin += pow(10, i);
+            }
+            }
+            
+            
+
+            
         }
-        
-        carryOn = false;
-        counter++;
+        else{
+            if((*bigBin)[longer-1-i] == '1' && (*smallBin)[shorter-1-i] == '1'){
+                //check if carryOn already true
+                if(carryOne > 0){
+                    buildBin += pow(10,i);
+                    if(i==longer-1){
+                        buildBin += pow(10,i+1);
+                    }
+                }
+                else if(carryOne == 0){
+                    if(i==longer-1){
+                        buildBin += pow(10, i+1);
+                    }
+                    carryOne++;
+                }
+                
+            }
+            else if((*bigBin)[longer-1-i] == '1' && (*smallBin)[shorter-1-i] == '0'){
+                if(carryOne == 0)
+                    buildBin += pow(10, i);
+                
+            }
+            else if((*bigBin)[longer-1-i] == '0' && (*smallBin)[shorter-1-i] == '1'){
+                if(carryOne == 0)
+                    buildBin += pow(10, i);
+            }
+            else{
+                if(carryOne == 1){
+                    buildBin += pow(10, i);
+                    carryOne --;
+                }
+                    
+            }
+
+
+        }
     }
-   
+    cout<<"buildBin: "<<buildBin<<"\n";
+    return to_string(buildBin);
 }
