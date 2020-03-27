447b60d674f1ee11a3bbbc6e4d2af465c184a7cd
Louis <louisglhuynh@gmail.com>
2020-03-27 19:35:49 -0400

    program able to use much larger binary inputs

--- cpp/67 - Add Binary/add_binary.cpp
+++ cpp/67 - Add Binary/add_binary.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-using std::string, std::to_string, std::cout;
+#include <vector>
+using std::string, std::to_string, std::cout, std::vector;
 
-/*
-checklist:
-
--if !carryOne && addOne -> add 1 -> both false
--if carryOne && addOne -> add 1 -> carryOne true, add false
--if carryOne && !addOne -> add 1 -> both false
--if !carryOne && !addOne -> add 0 -> both false 
-*/
 string addBinary(string a, string b) {int longer, shorter;
     string* bigBin;
     string* smallBin;
@@ -32,16 +25,19 @@ string addBinary(string a, string b) {int longer, shorter;
     
 
     int carryOne = 0;
-    long long buildBin;
+    vector<int> buildBin;
     for(int i=0;i<longer;i++){
         if(i>shorter-1){
             //only print long
             if(i==longer-1){
                 if((*bigBin)[longer-1-i] == '1' ){
-                    if(carryOne == 1)
-                        buildBin += pow(10, i+1);
+                    if(carryOne == 1){
+                        buildBin.push_back(0);
+                        buildBin.push_back(1);
+
+                    }
                     else{
-                        buildBin += pow(10, i);
+                        buildBin.push_back(1);
                     }
                 }
             
@@ -50,19 +46,20 @@ string addBinary(string a, string b) {int longer, shorter;
             else{
                 if((*bigBin)[longer-1-i] == '1'){
                     if(carryOne == 0)
-                        buildBin += pow(10, i);
+                        buildBin.push_back(1);
+
                     else{
-                        //do nothing
+                       buildBin.push_back(0);
                     }
                 }
                 else if((*bigBin)[longer-1-i] == '0'){
                     if(carryOne == 1){
-                        buildBin += pow(10, i);
+                        buildBin.push_back(1);
                         carryOne--;
                     }
     
                     else{
-                        //do nothing
+                        buildBin.push_back(0);
                     }
                 }
         
@@ -77,16 +74,18 @@ string addBinary(string a, string b) {int longer, shorter;
             if((*bigBin)[longer-1-i] == '1' && (*smallBin)[shorter-1-i] == '1'){
                 //check if carryOn already true
                 if(carryOne > 0){
-                    buildBin += pow(10,i);
+                    buildBin.push_back(1);
                     if(i==longer-1){
-                        buildBin += pow(10,i+1);
+                        buildBin.push_back(1);
                     }
                 }
                 else if(carryOne == 0){
                     if(i==longer-1){
-                        buildBin += pow(10, i+1);
+                        buildBin.push_back(0);
+                        buildBin.push_back(1);
                     }
                     else{
+                        buildBin.push_back(0);
                         carryOne++;
                     }
                 }
@@ -94,24 +93,34 @@ string addBinary(string a, string b) {int longer, shorter;
             }
             else if((*bigBin)[longer-1-i] == '1' && (*smallBin)[shorter-1-i] == '0'){
                 if(carryOne == 0)
-                    buildBin += pow(10, i);
+                    buildBin.push_back(1);
+                else
+                    buildBin.push_back(0);
                 
             }
             else if((*bigBin)[longer-1-i] == '0' && (*smallBin)[shorter-1-i] == '1'){
                 if(carryOne == 0)
-                    buildBin += pow(10, i);
+                    buildBin.push_back(1);
+                else
+                    buildBin.push_back(0);
             }
             else{
                 if(carryOne == 1){
-                    buildBin += pow(10, i);
+                    buildBin.push_back(1);
                     carryOne --;
                 }
+                else
+                    buildBin.push_back(0);
                     
             }
 
 
         }
     }
-    cout<<"buildBin: "<<buildBin<<"\n";
-    return to_string(buildBin);
+    string buildBin2 = "";
+    for(int i=0;i<buildBin.size();i++){
+        buildBin2.push_back('0'+ buildBin[buildBin.size()-1-i]);
+    }
+    cout<<"buildBin2: "<<buildBin2<<"\n";
+    return buildBin2;
 }
