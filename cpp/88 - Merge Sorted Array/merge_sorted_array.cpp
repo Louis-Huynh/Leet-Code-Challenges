0aa2a009ef9936cf7b2bed71267c84709f0cb5fc
Louis <louisglhuynh@gmail.com>
2020-03-31 19:09:23 -0400

    accesses out of bound values needs fix

--- cpp/88 - Merge Sorted Array/merge_sorted_array.cpp
+++ cpp/88 - Merge Sorted Array/merge_sorted_array.cpp
@@ -3,63 +3,102 @@
 #include <queue>
 using std::vector;
 
-
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
-    bool found=false;
-    int i = 0, j=0;
-    int temp;
+void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+    bool found = false;
+    int i = 0, j = 0;
     std::queue<int> holding;
-    bool finish;
+    bool finish = false;
 
-    while(!finish){
+    while (!finish)
+    {
+        if (n == 0)
+            break;
 
-        if(nums1[i] < nums2[j])
-            i++;
+        if (i >= nums1.size())
+        {
+            finish = true;
+            break;
+        }
 
-        else if(!holding.empty()){
+        else if (!holding.empty())
+        {
 
-            if(nums1[i] == 0){
-                if(holding.front() < nums2[j]){
-                    //place holding.front()
+            if (nums1.at(i) == 0)
+            {
+                if (j < n)
+                {
+                    if (holding.front() < nums2.at(j))
+                    {
+                        nums1.at(i) = holding.front();
+                        holding.pop();
+                        i++;
+                    }
+                    else
+                    {
+                        nums1.at(i) = nums2.at(j);
+                        i++;
+                        j++;
+                    }
                 }
-                else{
-                    //place nums2[j] into nums1[i]
+                else
+                {
+                    nums1.at(i) = holding.front();
+                    holding.pop();
+                    i++;
                 }
-                
             }
 
-            if(holding.front() == nums1[i]){
-                nums1[i+1] = holding.front();
-                holding.push(nums1[i+1]);
+            // else if (holding.front() == nums1.at(i))
+            // {
+            //     nums1.at(i+1) = holding.front();
+            //     holding.push(nums1.at(i+1));
+            //     holding.pop();
+            //     i++;
+            // }
+
+            else if (holding.front() < nums1.at(i) && holding.front() < nums2.at(j))
+            {
+                holding.push(nums1[i]);
+                nums1.at(i) = holding.front();
+                holding.pop();
                 i++;
             }
-            if(holding.front() < nums1[i] && holding.front() < nums2[j]){
-                holding.push(nums1[i]);
-                nums1[i] = holding.front();
-                holding.pop(); 
-                i++;
-            }
-            else if(nums2[j] < nums1[i]){
-                holding.push(nums1[i]);
-                nums1[i] = nums2[j];
+
+            else if (nums2.at(j) < nums1.at(i) && j < n)
+            {
+                holding.push(nums1.at(i));
+                nums1.at(i) = nums2.at(j);
                 i++;
                 j++;
             }
-            
+            else
+            {
+                holding.push(nums1.at(i));
+                nums1.at(i) = holding.front();
+                holding.pop();
+                i++;
+            }
         }
-    
-        //else if(nums1[i] == 0)
 
-        
-        else if(nums1[i] >= nums2[j]){
-            holding.push(nums1[i]);
-            nums1[i] = nums2[j];
+        else if (nums1.at(i) == 0)
+        {
+            nums1.at(i) = nums2.at(j);
             i++;
             j++;
         }
-    }
 
-    
-        
-    
+        else if (nums1.at(i) >= nums2.at(j))
+        {
+            holding.push(nums1[i]);
+            nums1.at(i) = nums2.at(j);
+            i++;
+            j++;
+        }
+
+        else
+        {
+            i++;
+        }
+    }
 }
